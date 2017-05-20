f47fd84f7746d9038276ceb8689eaa6cd8fb3278
gregory vincent <softtester19@gmail.com>
2017-05-21 01:07:26 +0530

    s

---
+++ POMExample/src/test/CallMethodArgDemo.java
@@ -0,0 +1,15 @@
+package test;
+
+public class CallMethodArgDemo {
+
+	public static void main(String[] args) {
+
+		MethodArgDemo m = new MethodArgDemo();
+		
+		String m1 = m.runQuery("12345");
+		
+		System.out.println(m1);
+		
+	}
+
+}
---
+++ POMExample/src/test/MethodArgDemo.java
@@ -0,0 +1,14 @@
+package test;
+
+public class MethodArgDemo {
+	
+	
+	public String runQuery(String query){
+		
+		String value = query;
+		
+		return value;
+		
+	}
+
+}
